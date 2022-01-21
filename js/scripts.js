8af4a4d7a3a7e1282151a9664c966e87ff478cb3
Nurgul Amat <nurgulamat212@gmail.com>
2022-01-20 22:34:59 -0600

    add display employee

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@
 // =========================================
 
 
-// Search container
+// Search container------------------------------------------------------------
 const searchContainer = document.querySelector('.search-container');
 const search = `
     <form action="#" method="get">
@@ -13,7 +13,49 @@ const search = `
     </form>
     `;
 searchContainer.insertAdjacentHTML('beforeend', search);
+ document.getElementById('search-input').focus();
 
 
+// Gallery markup---------------------------------------------------------------
+const gallery = document.getElementById('gallery');
+const urlAPI = "https://randomuser.me/api/?nat=us&results=12";
 
+// fetch data from API
+fetch(urlAPI)
+    .then(res => res.json())
+    .then(res => res.results)
+    .then(displayEmployees)
+    .catch(err => console.log(err))
+
+// displayEmployees function
+function displayEmployees(employeeData) {
+    employees = employeeData;
+    // console.log(employees.length);
+
+    // store the employee HTML as we create it
+    let employeeHTML = '';
+
+    // loop through each employee and create HTML markup
+    employees.forEach((employee, index) => {
+        let picture = employee.picture;
+        let name = employee.name;
+        let email = employee.email;
+        let city = employee.location.city;
+        let state = employee.location.state;
+       
+
+     employeeHTML = `
+        <div class="card" data-index="${index}">
+            <div class="card-img-container">
+                <img class="card-img" src="${picture.large}" alt="profile picture">
+            </div>
+            <div class="card-info-container">
+                <h3 id="${name}" class="card-name cap">${name.first} ${name.last}</h3>
+                <p class="card-email">${email}</p>
+                <p class="card-text cap">${city}, ${state}</p>
+            </div>
+        </div>
+    `});
+    gallery.insertAdjacentHTML('beforeend', employeeHTML);
+};
 
