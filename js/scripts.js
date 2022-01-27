ebc54c57dd709d942da53cef8eec35de04fd88c7
Nurgul Amat <nurgulamat212@gmail.com>
2022-01-27 09:06:10 -0600

    ready for slack review

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ function fetchData(urlAPI){
         .then(checkStatus)
         .then(response => response.json())
         .then(data => data.results.map(data => data))
-        .catch(err =>comsole.log(err));
+        .catch(err =>console.log(err));
 }
 fetchData(urlAPI).then(displayEmployees);
 
@@ -28,9 +28,49 @@ function checkStatus(response){
   }
   return response;
 }
-const gallery = document.querySelector('.gallery');
+
+ // Search container===========================================================================
+ const searchContainer = document.querySelector('.search-container');
+ const searchHTML = `
+     <form action="#" method="get">
+         <input type="search" id="search-input" class="search-input" placeholder="Search...">
+         <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+     </form>
+     `;
+ searchContainer.insertAdjacentHTML('beforeend', searchHTML);
+ 
+ 
+ 
+ // searchFilter function===================================================================== 
+ function searchFilter(e) {
+     let searchName = e.target.value.toLowerCase();   
+     let employeeNames = document.querySelectorAll(".card-name");
+     // console.log(employeeNames.length)
+ 
+     employeeNames.forEach(employeeName => {
+          let name =employeeName.textContent.toLowerCase();
+          let nameOfEmployee = employeeName.parentElement.parentElement;
+         
+       if(name.includes(searchName)){
+         nameOfEmployee.style.display = "";
+        
+       } else {
+         
+         nameOfEmployee.style.display = "none";
+       }
+       
+     });
+ }
+ 
+ const search = document.getElementById("search-input");
+ search.addEventListener('input', searchFilter);
+
+
 // displayEmployees Function=========================================================
+const gallery = document.querySelector('.gallery');
+
 function displayEmployees(employeeData){
+    // console.log(employeeData)
 
         // loop through each employee and create HTML markup
         employeeData.forEach((employee, index) =>{
@@ -44,9 +84,9 @@ function displayEmployees(employeeData){
             let city = employee.location.city;
             let state = employee.location.state;
 
-            let employeeHTML = '';
+           
             // template literals make this so much cleaner!
-            employeeHTML += `
+          let employeeHTML = `
                 <div class="card-img-container">
                     <img class="card-img" src="${picture}" alt="">
                 </div>
@@ -59,37 +99,41 @@ function displayEmployees(employeeData){
 
             card.innerHTML= employeeHTML;
             card.addEventListener("click", () => {
-            displayModal(employee, index);
+            displayModal(employeeData, index);
+            // console.log(employeeData, index);
         });    
     });
 }
 
 
 // displayModal=======================================================================
-function displayModal(employee, index){
-    const selectedEmployee = employee[index];
+function displayModal(employeeData, index){
+  const employee = employeeData[index];
+//   console.log(employeeData.length, index)
+  let picture = employee.picture;
+  let name = employee.name;
+  let email = employee.email;
+  let city = employee.location.city;
+  let state = employee.location.state;
+  let phone = employee.phone;
+  let street = employee.location.street;
+  let postcode = employee.location.postcode;
+
+    let date = new Date(employee.dob.date);
+
     // create modal-container div and set the calss name as 'modal-container'
     const modalContainer = document.createElement('div');
     modalContainer.className = 'modal-container';
     // append it to gallery
     gallery.append(modalContainer);
 
-    let picture = employee.picture.large;
-    let name = employee.name;
-    let email = employee.email;
-    let city = employee.location.city;
-    let state = employee.location.state;
-    let phone = employee.phone;
-    let street = employee.location.street;
-    let postcode = employee.location.postcode;
-    let date = new Date(employee.dob.date);
-
-    const modalHTML = `
+   
+   let modalHTML = `
         <div class="modal">
             <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
             <div class="modal-info-container">
-                <img class="modal-img" src="${picture}" alt="profile picture">
-                <h3 id="${name}" class="modal-name cap">${name.first} ${name.last}</h3>
+                <img class="modal-img" src="${picture.large}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${name.first} ${name.last}</h3>
                 <p class="modal-text">${email}</p>
                 <p class="modal-text cap">${city}</p>
 
@@ -109,47 +153,52 @@ function displayModal(employee, index){
     `;
     modalContainer.innerHTML = modalHTML;
 
-   // modalClose click event : add the hidden class to the modal 
-   const modalCloseBtn = document.querySelector(".modal-close-btn");
+   // modalClose click event : add the hidden class to the modalContainer 
+    const modalCloseBtn = document.querySelector(".modal-close-btn");
     modalCloseBtn.addEventListener('click', () => {
-        modalContainer.classList.add('hidden');
+        modalContainer.remove();
+    });
+
+
+    const modalNext = document.getElementById("modal-next");
+    const modalPrev = document.getElementById("modal-prev");
+ // the following eventListener is to switch back and forth between employees when the detail modal window is open
+
+
+    modalNext.addEventListener("click", (e) => {
+        // console.log(employeeData.length)
+        if (index >= 0 && index < employeeData.length-1) {
+            modalContainer.remove();
+            displayModal(employeeData,index+1);
+        } else if (index === employeeData.length-1) {
+            modalContainer.remove();
+            index = 0;
+            displayModal(employeeData,index);
+        }
+    });
+
+    modalPrev.addEventListener("click", (e) => {
+        if (index > 0 && index <= employeeData.length-1) {
+            modalContainer.remove();
+            displayModal(employeeData,index-1);
+        } else if (index === 0) {
+            modalContainer.remove();
+            index = employeeData.length-1;
+            displayModal(employeeData,index);
+        }
     });
 
 }
 
- // Search container------------------------------------------------------------
-const searchContainer = document.querySelector('.search-container');
-const searchHTML = `
-    <form action="#" method="get">
-        <input type="search" id="search-input" class="search-input" placeholder="Search...">
-        <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
-    </form>
-    `;
-searchContainer.insertAdjacentHTML('beforeend', searchHTML);
+
+  
 
 
-// // ------------------------------------------------------------------------
-// //   searchFilter function
-// // ------------------------------------------------------------------------ 
-function searchFilter(e) {
-    let searchName = e.target.value.toLowerCase();   
-    let employeeNames = document.querySelectorAll(".card-name");
-    // console.log(employeeNames.length)
 
-    employeeNames.forEach(employeeName => {
-         let name =employeeName.textContent.toLowerCase();
-         let nameOfEmployee = employeeName.parentElement.parentElement;
-        
-      if(name.includes(searchName)){
-        nameOfEmployee.style.display = "";
-       
-      } else {
-        
-        nameOfEmployee.style.display = "none";
-      }
-      
-    });
-}
 
-const search = document.getElementById("search-input");
-search.addEventListener('input', searchFilter);
+
+
+
+
+
+
